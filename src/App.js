8ccdeefc3bb227c9a83cf78236e87feaca6dbcbb
Enryahhh <linggaj28@gmail.com>
2022-07-12 10:45:14 +0700

    learn basic and create tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import React, { Component } from 'react';
+import Game from './TicTacToe'
 import logo from './logo.svg';
 import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+class Coba extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {tulisan:'',nama:"LING"};
+
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+  }
+
+  handleKeyUp(event){
+    console.log("asd")
+    this.setState({tulisan:event.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.tulisan} onChange={this.handleKeyUp}/>
+        <p>{this.state.tulisan}</p>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome name="Lingjul" />
+        <Game/>
       </header>
     </div>
   );
